59b7e79fd4d799880739894783b103d4787793ac
Eylül Akbaş <eylul.akbas@bil.omu.edu.tr>
2013-12-25 16:58:38 +0200

    kırp

--- python/euler2.py
+++ python/euler2.py
@@ -1,13 +1,10 @@
 def fib():
-	artis=1
-	toplam=0
-	sayi=0
+	artis,toplam,sayi=1,0,0
 	while True:
 		sayi=sayi+artis
 		if sayi <= 4000000:
 			if sayi%2==0:
 				toplam+=sayi
-			
 		else:
 			break
 		artis=sayi-artis
