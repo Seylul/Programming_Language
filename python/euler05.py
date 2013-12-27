a0f85fa76b83f4457fa15babdcd3b8b8276176c8
Eylül Akbaş <eylul.akbas@bil.omu.edu.tr>
2013-12-27 11:08:08 +0200

    one line

---
+++ python/euler05.py
@@ -0,0 +1,5 @@
+#~ largest number small from 20 is 19 and its prime. we must collect all prime numbers as:
+#~ 19,17,13,11,7,5
+#~ and small from 20 and the largest even of 2 and 3 are 16 and 9.
+#~ so we must multiply all of them its the result of this question.
+print 19*17*16*13*11*9*7*5
